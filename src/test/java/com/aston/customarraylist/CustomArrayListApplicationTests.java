27d0242a23205c76b7ba6d93d04a38828173692e
Anton <Antosah1@mail.ru>
2022-07-27 20:59:46 +0300

    init commit

---
+++ src/test/java/com/aston/customarraylist/CustomArrayListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aston.customarraylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomArrayListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
